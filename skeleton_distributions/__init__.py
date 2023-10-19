73dc51b9315e16e5d2e2667a552288c81cb68378
Alex_Mordhorst <alexander.mordhorst@icloud.com>
2023-10-19 18:48:27 +0200

    c

---
+++ skeleton_distributions/__init__.py
@@ -0,0 +1,6 @@
+from .skeletons import ProbDist
+from .skeletons import UnivPD
+from .skeletons import MultivPD
+from .skeletons import DUPD
+from .skeletons import BinomialDistribution
+from .skeletons import NegBinomialDistribution
